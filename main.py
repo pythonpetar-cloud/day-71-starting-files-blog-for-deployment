2fbded076d7e93d5dfddec80976ec75b89ba4265
Petarhol <petarholi@gmail.com>
2026-03-09 02:22:55 +0100

    test to see if I changed git account

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 import os
 
-
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 
